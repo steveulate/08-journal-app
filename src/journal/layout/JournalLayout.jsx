d2b27f01eebc8fcb7b7cc40d8fe359df507b5102
Steve <steve_ulate@hotmail.com>
2023-10-11 10:20:56 -0600

    FIRST COMMIT

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,25 @@
+import { Box, Toolbar } from "@mui/material";
+import { Navbar, SideBar } from "../components";
+
+const drawerWidth = 280;
+
+export const JournalLayout = ({ children }) => {
+    return (
+        <Box sx={{display: 'flex'}}>
+
+            <Navbar drawerWidth={drawerWidth}/>
+
+            <SideBar drawerWidth={drawerWidth}/>
+
+
+            <Box 
+                component='main'
+                sx={{flexGrow: 1, p: 3}}
+            >
+                <Toolbar/>
+                {children}
+            </Box>
+
+        </Box>
+    );
+}
